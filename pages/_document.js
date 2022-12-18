d4ed6d30943fb0fd409f79356bf1f0d9e24521a1
İlke Aşağıçayır <ilke.asagicayir@gmail.com>
2022-12-18 23:42:51 +0300

    header

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="True" />
+        <link href="https://fonts.googleapis.com/css2family=Dancing+Script:wght@400;700&family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
