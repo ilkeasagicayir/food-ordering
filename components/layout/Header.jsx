3df65f49259dda1fe81598233f8505bdf59cf4a9
İlke Aşağıçayır <ilke.asagicayir@gmail.com>
2022-12-19 23:01:01 +0300

    Update Header.jsx
    Responsive Menu Component

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -2,31 +2,40 @@ import { useState } from "react"
 import Logo from "../ui/Logo"
 import {FaUserAlt, FaShoppingCart, FaSearch} from "react-icons/fa"
 import Search from "../ui/Search"
+import {GiHamburgerMenu, GiCancel} from "react-icons/gi"
 
 const Header = () => {
   const [isSearchModal, setIsSearchModal] = useState(false) 
+  const [isMenuModal, setIsMenuModal] = useState(false) 
 
   return (
     <div className="h-[5.5rem] bg-secondary">
         <div className="container mx-auto text-white  flex justify-between items-center h-full">
-        <div>
-            <Logo></Logo>
-        </div>
-        <nav>
-            <ul className="flex gap-x-2">
+            <Logo />
+        <nav className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-full sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden ${isMenuModal === true && "!grid place-content-center"}`}>
+            <ul className="flex gap-x-2 sm:flex-row flex-col items-center">
                 <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer"><a href="">Home</a></li>
                 <li className="px-[5px] py-[10px] uppercase hover:text-primary  cursor-pointer"><a href="">Menu</a></li>
                 <li className="px-[5px] py-[10px] uppercase hover:text-primary  cursor-pointer"><a href="">About</a></li>
                 <li className="px-[5px] py-[10px] uppercase hover:text-primary  cursor-pointer"><a href="">Book A Table</a></li>
             </ul>
+            {isMenuModal && (
+                <button className='absolute top-4 right-4 z-50' 
+                onClick={() => setIsMenuModal(false)}>
+                <GiCancel size={25} className=' transition-all'></GiCancel>
+            </button>
+            )}
         </nav>
         <div className="flex gap-x-4 items-center">
             <a href="#"><FaUserAlt className="hover:text-primary transition-all"/></a>
             <a href="#"><FaShoppingCart className="hover:text-primary transition-all"/></a>
             <button onClick={() => setIsSearchModal(true)}><FaSearch className="hover:text-primary transition-all"/></button>
-            <a href="#">
+            <a href="#" className="md:inline-block hidden">
                 <button className="btn-primary">Order Online</button>
             </a>
+            <button className="sm:hidden inline-block" onClick={()=>setIsMenuModal(true)}>
+                <GiHamburgerMenu className="text-xl hover:text-primary transition-all" />
+            </button>
         </div>
         </div>
         {isSearchModal && <Search setIsSearchModal={setIsSearchModal}/>}
