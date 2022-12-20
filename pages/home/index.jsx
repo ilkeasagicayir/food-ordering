534cdbc468c14970e800f71722eec9ee8c9f0fb3
İlke Aşağıçayır <ilke.asagicayir@gmail.com>
2022-12-20 17:53:55 +0300

    About component

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MenuWrapper from '../../components/product/MenuWrapper'
+import About from '../../components/ui/About'
 import Campaigns from '../../components/ui/Campaigns'
 import Carousel from '../../components/ui/Carousel'
 
@@ -9,6 +10,7 @@ const Index = () => {
     <Carousel/>
     <Campaigns/>
     <MenuWrapper/>
+    <About />
   </div>
   )
 }
