c3d81c60fab79d0ff940d8182d187a82287cb481
İlke Aşağıçayır <ilke.asagicayir@gmail.com>
2023-01-03 18:05:30 +0300

    Product Details and Prices

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -1,13 +1,16 @@
-import Image from "next/image"
-import {FaShoppingCart} from "react-icons/fa"
+import Image from "next/image";
+import Link from "next/link";
+import { FaShoppingCart } from "react-icons/fa";
 
 const MenuItem = () => {
   return (
     <div className="bg-secondary rounded-3xl">
       <div className="w-full  bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
-        <div className="relative w-36 h-36 hover:scale-110 transition-all">
-          <Image src="/images/f1.png" alt="" layout="fill" />
-        </div>
+        <Link href="/product">
+          <div className="relative w-36 h-36 hover:scale-110 transition-all">
+            <Image src="/images/f1.png" alt="" layout="fill" />
+          </div>
+        </Link>
       </div>
       <div className="p-[25px] text-white">
         <h4 className="text-xl font-semibold">Delicious Hamburger</h4>
@@ -23,7 +26,7 @@ const MenuItem = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MenuItem
+export default MenuItem;
