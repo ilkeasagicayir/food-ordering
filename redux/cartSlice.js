f5dc9719678d63430b2d54a39d1193dcce8ff739
İlke Aşağıçayır <ilke.asagicayir@gmail.com>
2023-01-03 20:04:59 +0300

    addProduct ve reset actions on Cart
    Redux-toolkit

--- redux/cartSlice.js
+++ redux/cartSlice.js
@@ -13,8 +13,13 @@ const cartSlice = createSlice({
             state.quantity += action.payload.quantity;
             state.total += action.payload.price;
         },
+        reset: (state, action) => {
+            state.products = [];
+            state.quantity = 0;
+            state.total = 0;
+        },
     },
 });
 
-export const { addProducts } = cartSlice.actions;
+export const { addProducts, reset } = cartSlice.actions;
 export default cartSlice.reducer;
