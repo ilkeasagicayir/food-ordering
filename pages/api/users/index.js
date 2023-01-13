cbcd3887b88d618d65ff210273c4bb7ca8abcf8a
İlke Aşağıçayır <ilke.asagicayir@gmail.com>
2023-01-13 10:48:39 +0300

    Update database

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -19,6 +19,8 @@ const handler = async (req, res) => {
     try {
       const newUser = await User.create(req.body);
       res.status(200).json(newUser);
+      
+  console.log(newUser)
     } catch (error) {
       console.log(error);
     }
