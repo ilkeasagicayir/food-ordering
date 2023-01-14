ca8ce4280aaa2c174dff83318d43549d0804ad07
İlke Aşağıçayır <ilke.asagicayir@gmail.com>
2023-01-14 16:30:41 +0300

    AddProduct component

--- pages/admin/profile.jsx
+++ pages/admin/profile.jsx
@@ -7,11 +7,13 @@ import Footer from "../../components/admin/Footer";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
+import AddProduct from "../../components/admin/AddProduct";
 
 const Profile = () => {
   const [tabs, setTabs] = useState(0);
+  const [isProductModal, setIsProductModal] = useState(false);
 
-  const {push} = useRouter();
+  const { push } = useRouter();
 
   const closeAdminAccount = async () => {
     try {
@@ -92,6 +94,13 @@ const Profile = () => {
       {tabs === 1 && <Order />}
       {tabs === 2 && <Category />}
       {tabs === 3 && <Footer />}
+      {isProductModal && <AddProduct setIsProductModal={setIsProductModal} />}
+      <button
+        className="btn-primary !w-12 !h-12 !p-0 absolute bottom-14 right-10 text-4xl"
+        onClick={() => setIsProductModal(true)}
+      >
+        +
+      </button>
     </div>
   );
 };
